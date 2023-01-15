e942332c2a6243bd11f1028a7258600df8577855
Krzysiek <krzysztof.g.git@gmail.com>
2023-01-15 16:19:42 +0100

    Create test without api test.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 from string import digits, punctuation, ascii_uppercase, ascii_lowercase
+from hashlib import sha1
+from requests import get
 
 
 class WeakPassword(Exception):
@@ -70,22 +72,31 @@ class HasLowerCharacterValidator(Validator):
 
 
 class LengthValidator(Validator):
-    """Check password length: password (str) """
+    """Check password length: password (str) min length is 8 char"""
     def __init__(self, password):
         self.password = password
 
     def is_valid(self):
-        if len(self.password) < 7:
+        if len(self.password) < 8:
             raise WeakPassword('Too short password')
+        else:
+            return True
 
 
 class Have_I_been_Pwd_Validator(Validator):
     """Check leak password in network: password (str) """
-    def __init__(self, password):
+    def __init__(self, password: str):
         self.password = password
+        self.url = 'https://api.pwnedpasswords.com/range/'
 
     def is_valid(self):
-        pass
+        password = sha1(self.password.encode('utf-8')).hexdigest().upper()
+        responce = get(self.url+password[:5]).text.splitlines()
+        for single_responce in responce:
+            hash = single_responce.split(':')[0]
+            numbers_of_leaks = single_responce.split(':')[1]
+            if hash == password[5:]:
+                raise WeakPassword(f'Yours password has been leaked {numbers_of_leaks} times.')
 
 
 class PasswordValidator(Validator):
@@ -108,4 +119,33 @@ class PasswordValidator(Validator):
             self.check_rules.append(validator.is_valid())
 
             if all(self.check_rules):
+                print(self.password)
                 raise StrongPassword('Password is strong')
+class LoadPassword:
+    def __init__(self):
+        self.passwords = str
+        self.path_passowrd = 'Fake password.txt'
+        self.path_validate_password = 'Validate password.txt'
+        self.get()
+    def get(self):
+        with open(self.path_passowrd,'r') as readfile, open(self.path_validate_password,'a') as validateFile:
+           # validateFile.truncate(0)
+            self.passwords = readfile.readlines()
+
+            for password in self.passwords:
+                try:
+                    password = password.strip('\n')
+                    print(f'Check {password}')
+                    PasswordValidator(password).is_valid()
+                except WeakPassword as msg:
+                    print(f'{password}  ' + str(msg))
+                    print('_________________')
+
+                except StrongPassword as msg:
+                    print(f'Password {password} is strong.')
+                    validateFile.writelines(f'Password: {password} is strong.')
+
+
+
+if __name__ == '__main__':
+    LoadPassword()
